072ce1af173a83032f30d466e5b9587921d44809
Olga Panfilova <gr-olga@ya.ru>
2024-07-14 15:40:29 +0200

    simplification

--- src/utils/createHierarchy.ts
+++ src/utils/createHierarchy.ts
@@ -1,23 +1,26 @@
 import type { IHierarchy } from '@/types/dataTypes'
 
-export function createHierarchy(items: ReadonlyArray<IHierarchy>) {
-  const itemMap: Map<string, IHierarchy> = new Map()
+export function createHierarchy(items: ReadonlyArray<IHierarchy>): Array<IHierarchy> {
+  const itemMap: Map<string, IHierarchy> = new Map();
+  const rootItems: Array<IHierarchy> = [];
 
   items.forEach((item: IHierarchy) => {
-    item.children = []
-    itemMap.set(item.name, item)
-  })
+    item.children = [];
+    itemMap.set(item.name, item);
 
-  const rootItems: Array<IHierarchy> = []
-
-  items.forEach((item: IHierarchy) => {
-    if (item.parent === '') {
-      rootItems.push(item)
+    if (!hasParent(item)) {
+      rootItems.push(item);
     } else {
-      const parentItem: IHierarchy | undefined = itemMap.get(item.parent)
-      if (parentItem && parentItem.children) parentItem.children.push(item)
+      const parentItem: IHierarchy | undefined = itemMap.get(item.parent);
+      if (parentItem && parentItem.children) {
+        parentItem.children.push(item);
+      }
     }
-  })
+  });
 
-  return rootItems
+  return rootItems;
+}
+
+function hasParent(item: IHierarchy): boolean {
+  return item.parent !== '';
 }
