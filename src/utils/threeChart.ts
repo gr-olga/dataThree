c7969a560386ef0da32b00bcb6a73fa66bb248c1
Olga Panfilova <gr-olga@ya.ru>
2024-07-14 15:25:05 +0200

    fixed d3 types

--- src/utils/threeChart.ts
+++ src/utils/threeChart.ts
@@ -1,68 +1,63 @@
-import type { IDataType, IHierarchy } from '@/types/dataTypes'
+import type {IHierarchy} from '@/types/dataTypes'
+import type {HierarchyPointNode} from 'd3';
 import * as d3 from 'd3'
-import type { HierarchyNode } from 'd3'
 
 export function treeChart(
-  container: HTMLElement,
-  data: ReadonlyArray<IHierarchy>,
-  onClick: (name: string, description: string) => void
+    container: HTMLElement,
+    data: ReadonlyArray<IHierarchy>,
+    onClick: (name: string, description: string) => void
 ) {
-  const width: number = 600
-  const height: number = 400
+    const width: number = 600
+    const height: number = 400
 
-  const treeLayout = d3.tree().size([height, width - 160])
+    const treeLayout = d3.tree<IHierarchy>().size([height, width - 160])
 
-  const root = d3.hierarchy(data[0])
-  treeLayout(root)
+    const root: HierarchyPointNode<IHierarchy> = treeLayout(d3.hierarchy<IHierarchy>(data[0]))
 
-  const svg = d3.select(container).append('svg').attr('width', width).attr('height', height)
+    const svg = d3.select(container).append('svg').attr('width', width).attr('height', height)
 
-  const g = svg.append('g').attr('transform', 'translate(80,0)')
+    const g = svg.append('g').attr('transform', 'translate(80,0)')
 
-  g.selectAll('.link')
-    .data(root.descendants().slice(1))
-    .enter()
-    .append('path')
-    .attr('class', 'link')
-    .attr(
-      'd',
-      (d: HierarchyNode<IDataType>) => `
+    g.selectAll('.link')
+        .data(root.descendants().slice(1))
+        .enter()
+        .append('path')
+        .attr('class', 'link')
+        .attr(
+            'd',
+            (d: HierarchyPointNode<IHierarchy>) => `
       M${d.y},${d.x}
-      C${d.parent.y + 100},${d.x}
-      ${d.parent.y + 100},${d.parent.x}
-      ${d.parent.y},${d.parent.x}
-    `
-    )
+      C${d.parent!.y + 100},${d.x}
+      ${d.parent!.y + 100},${d.parent!.x}
+      ${d.parent!.y},${d.parent!.x}
+    `);
 
-  const node = g
-    .selectAll('.node')
-    .data(root.descendants())
-    .enter()
-    .append('g')
-    .attr(
-      'class',
-      (d: HierarchyNode<IDataType>) => 'node' + (d.children ? ' node--internal' : ' node--leaf')
-    )
-    .attr('transform', (d: HierarchyNode<IDataType>) => `translate(${d.y},${d.x})`)
-    .on('click', (event: any, d: HierarchyNode<IDataType>) =>
-      onClick(d.data.name, d.data.description)
-    )
+    const node = g
+        .selectAll('.node')
+        .data(root.descendants())
+        .enter()
+        .append('g')
+        .attr(
+            'class',
+            (d: HierarchyPointNode<IHierarchy>) =>
+                'node' + (d.children ? ' node--internal' : ' node--leaf')
+        )
+        .attr('transform', (d: HierarchyPointNode<IHierarchy>) => `translate(${d.y},${d.x})`)
+        .on('click', (event: MouseEvent, d: HierarchyPointNode<IHierarchy>) => onClick(d.data.name, d.data.description));
 
-  node
-    .append('rect')
-    .attr('width', 80)
-    .attr('height', 30)
-    .attr('x', -40)
-    .attr('y', -15)
-    .style('fill', 'white')
-    .style('stroke', 'black')
+    node
+        .append('rect')
+        .attr('width', 80)
+        .attr('height', 30)
+        .attr('x', -40)
+        .attr('y', -15)
+        .style('fill', 'white')
+        .style('stroke', 'black');
 
-  node
-    .append('text')
-    .attr('dy', 3)
-    .attr('x', (d: HierarchyNode<IDataType>) => (d.data.children ? -8 : 8))
-    .attr('text-anchor', (d: HierarchyNode<IDataType>) => {
-      return d.data.children.name ? 'end' : 'start'
-    })
-    .text((d: HierarchyNode<IDataType>) => d.data.name)
+    node
+        .append('text')
+        .attr('dy', 3)
+        .attr('x', 0)
+        .attr('text-anchor', 'middle')
+        .text((d: HierarchyPointNode<IHierarchy>) => d.data.name);
 }
